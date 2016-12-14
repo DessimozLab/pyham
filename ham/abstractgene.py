8ca1b1995a73296fa03a5d936348efc16c2387f6
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2016-12-14 23:31:55 +0100

    re-adding abstractgene.py that got removed before while doing a rename before

---
+++ ham/abstractgene.py
@@ -0,0 +1,66 @@
+__author__ = 'admin'
+
+from abc import ABCMeta, abstractmethod
+
+
+class AbstractGene(metaclass=ABCMeta):
+    def __init__(self):
+        self.parent = None
+        self.taxon = None
+
+    @abstractmethod
+    def set_taxon_range(self, tax):
+        """Set the taxonomic range"""
+        pass
+
+
+class HOG(AbstractGene):
+
+    def __init__(self, id=None, **kwargs):
+        super(HOG, self).__init__()
+        self.hog_id = id
+        self.depth = 0
+        self.children = []
+
+    def add_child(self, elem):
+        if not isinstance(elem, AbstractGene):
+            raise TypeError("expect subclass obj of '{}', got {}"
+                            .format(AbstractGene.__name__,
+                                    type(elem).__name__))
+        if self == elem:
+            raise EvolutionaryConceptError('Cannot be a child of itself')
+        self.children.append(elem)
+        elem.parent = self
+
+    def append(self, elem):
+        self.add_child(elem)
+
+    def set_taxon_range(self, tax):
+        """A HOG can potentially belong to several taxonomic ranges"""
+        if self.taxon is None:
+            self.taxon = set([])
+        self.taxon.add(tax)
+
+    def __repr__(self):
+        return "<{}({})>".format(self.__class__.__name__, self.hog_id if self.hog_id else "")
+
+
+class Gene(AbstractGene):
+    def __init__(self, id, geneId=None, protId=None, transcriptId=None, **kwargs):
+        super(Gene, self).__init__()
+        self.unique_id = id
+        self.gene_id = geneId
+        self.prot_id = protId
+        self.transcript_id = transcriptId
+
+    def set_taxon_range(self, tax):
+        if self.taxon is not None and tax != self.taxon:
+            raise EvolutionaryConceptError("Gene can only belong to one genome")
+        self.taxon = tax
+
+    def __repr__(self):
+        return "{}({}{})".format(self.__class__.__name__, self.unique_id)
+
+
+class EvolutionaryConceptError(Exception):
+    pass
