e5610635e1d1749064b8a6ce20ea5978b62d8316
ctrain <clement.train@gmail.com>
2018-08-01 18:20:19 +0200

    add basit UT that iHam can run on good example

---
+++ tests/test_iham.py
@@ -0,0 +1,21 @@
+import unittest
+from pyham import ham, utils, iham
+import os
+
+class IHAMTest(unittest.TestCase):
+
+    def setUp(self):
+
+        nwk_path = os.path.join(os.path.dirname(__file__), './data/simpleEx.nwk')
+        tree_str = utils.get_newick_string(nwk_path, type="nwk")
+        orthoxml_path = os.path.join(os.path.dirname(__file__), './data/simpleEx.orthoxml')
+
+        self.ham_analysis = ham.Ham(newick_str=tree_str, hog_file=orthoxml_path, type_hog_file='orthoxml',
+                                    use_internal_name=True)
+
+    def test_ok(self):
+
+        hog_3 = self.ham_analysis.get_hog_by_id(1)
+        self.ham_analysis.create_iHam(hog=hog_3)
+
+
